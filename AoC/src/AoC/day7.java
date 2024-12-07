7d779c985a3c9b13417ce4ed489f67f5cf557a7b
DuodecimAD <tonyxii@gmail.com>
2024-12-07 17:13:53 +0100

    AoC 7.2 done (should understand problem before making it more difficult)

--- AoC/src/AoC/day7.java
+++ AoC/src/AoC/day7.java
@@ -10,7 +10,8 @@ public class day7 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//part1();
-		part2();
+		//part2();
+		part2ForReal();
 
 	}
 	
@@ -151,6 +152,73 @@ public class day7 {
 		            } else {
 		            	numbersConcat++;
 		            }
+		            
+//		            System.out.print(" " + operator + " ");
+//	                System.out.print(monArray.get(j));
+                
+				}
+//				System.out.print(" = " + somme);
+//				System.out.println();
+				
+				if (somme == checkValue) {
+					result += somme;
+                	break;
+                }
+				
+			}
+
+        }
+        System.out.println("\nresultat final = " + result);
+	}
+	
+	public static void part2ForReal() {
+		
+		char[] operators = {'+', '*', '|'};
+		String[] string = Fichier.lireLignes("data/ex7Input2.txt");
+		ArrayList<String[]> stringArray = new ArrayList<String[]>();
+		long result = 0;
+		
+		for (int i = 0; i < string.length; i++) {
+			stringArray.add(string[i].split(" "));
+		}
+        
+        for (int t = 0; t < string.length; t++) {
+        	ArrayList<Integer> monArray = new ArrayList<Integer>();
+        	        	
+//      	System.out.println("\n" + Arrays.toString(stringArray.get(t)));
+        	long checkValue = Long.parseLong(stringArray.get(t)[0].substring(0, stringArray.get(t)[0].length()-1));
+//     		System.out.println("checkValue = " +checkValue);
+        	int lengthy = stringArray.get(t).length;
+        	for (int i = 1; i < lengthy; i++) {
+        		monArray.add(Integer.parseInt(stringArray.get(t)[i]));
+        		
+			}
+//       	System.out.println("monArray = " +monArray);
+        	int numOperators = monArray.size() - 1;
+        	int totalCombinations = (int) Math.pow(operators.length, numOperators);
+			
+			for (int i = 0; i < totalCombinations; i++) {
+				long somme = monArray.get(0);
+				int temp = i;
+//				System.out.print(somme);
+				char lastOperator = 0;
+				int numbersConcat = 2;
+				long lastChiffreAColler = 0;
+				int k = 0;
+				
+				for (int j = 1; j < monArray.size(); j++) {
+					
+					char operator = operators[temp % operators.length]; // Merci ChatGPT mais la logique 
+		            temp /= operators.length;							// me passe un peu au dessus de la tête :'(
+					
+		            if (operator == '+') {
+	                    somme += monArray.get(j);
+	                } else if (operator == '*') {
+	                    somme *= monArray.get(j);
+	                } else if (operator == '|' ) {
+	                	long chiffreAColler = monArray.get(j);
+	                	somme = Long.parseLong(somme +  "" + chiffreAColler);
+	                } 
 		            
 //		            System.out.print(" " + operator + " ");
 //	                System.out.print(monArray.get(j));
